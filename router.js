5d1d089f6a6750a94e93a8e56a6e1223efe7d26d
hirenkumar <hirenkumar@me.com>
2018-12-23 21:50:12 +1100

    new router.js file created

---
+++ router.js
@@ -0,0 +1,18 @@
+
+
+//creating hello function
+function hello(req,res){
+  var mysite = "www.hirenkumar.com.au";
+  var username = req.url.replace("/","");
+  if(username.length>0){
+     if(username==="hello"){
+       res.writeHead(200,{'Content-Type':"text/plain"});
+       res.write("Hi guyies :: \n");
+       red.write(mysite +"\n");
+       res.end("Thank you \n");
+     }
+  }
+};
+
+//exporting module
+module.exports.hello = hello;
